b3d6bb009f27dee7835c637495922144daefd72b
Chen <victorchennn@gmail.com>
2020-05-19 13:53:18 -0400

    update facebook

--- Companies/Facebook/MaximumSumof3NonOverlappingSubarrays.java
+++ Companies/Facebook/MaximumSumof3NonOverlappingSubarrays.java
@@ -1,8 +1,24 @@
 package Companies.Facebook;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Given an array of positive integers, find three non-overlapping subarrays with maximum sum.
+ * Each subarray will be of size k, and we want to maximize the sum of all 3*k entries.
+ *
+ * Input: [1,2,1,2,6,7,5,1], 2
+ * Output: [0,3,5]
+ *
+ * Explanation: Subarrays [1, 2], [2, 6], [7, 5] correspond to the starting indices [0, 3, 5].
+ * We could have also taken [2, 1], but an answer of [1, 3, 5] would be lexicographically larger.
+ */
 public class MaximumSumof3NonOverlappingSubarrays {
     public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
-        int[] wins = new int[nums.length-k+1];
+        int[] wins = new int[nums.length-k+1];  // window sum
         int curSum = 0;
         for (int i = 0; i < nums.length; i++) {
             curSum += nums[i];
@@ -25,7 +41,7 @@ public class MaximumSumof3NonOverlappingSubarrays {
         }
         p = wins.length-1;
         for (int i = wins.length-1; i >= 0; i--) {
-            if (wins[i] >= wins[p]) {
+            if (wins[i] >= wins[p]) { // need the lexicographically smallest one
                 p = i;
             }
             rMax[i] = p;
@@ -41,5 +57,11 @@ public class MaximumSumof3NonOverlappingSubarrays {
             }
         }
         return re;
+    }
+
+    @Test
+    void test() {
+        assertEquals(true, Arrays.equals(new int[]{0,3,5},
+                maxSumOfThreeSubarrays(new int[]{1,2,1,2,6,7,5,1}, 2)));
     }
 }
