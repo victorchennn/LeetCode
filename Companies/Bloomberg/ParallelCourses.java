28a7e2172bc5e3e762f203451142edfb956298a3
Victor <36978599+victorchennn@users.noreply.github.com>
2026-07-17 13:56:33 -0400

    Create ParallelCourses.java

---
+++ Companies/Bloomberg/ParallelCourses.java
@@ -0,0 +1,40 @@
+public int minimumSemesters(int n, int[][] relations) {
+        int[] inCount = new int[N + 1]; // 或者入度
+        List<List<Integer>> graph = new ArrayList<>(N + 1);
+        for (int i = 0; i < N + 1; ++i) {
+            graph.add(new ArrayList<Integer>());
+        }
+        for (int[] relation : relations) {
+            graph.get(relation[0]).add(relation[1]);
+            inCount[relation[1]]++;
+        }
+        int step = 0;
+        int studiedCount = 0;
+        List<Integer> bfsQueue = new ArrayList<>();
+        for (int node = 1; node < N + 1; node++) {
+            if (inCount[node] == 0) {
+                bfsQueue.add(node);
+            }
+        }
+        // 开始使用 BFS 学习
+        while (!bfsQueue.isEmpty()) {
+            // 开始一个新学期
+            step++;
+            List<Integer> nextQueue = new ArrayList<>();
+            for (int node : bfsQueue) {
+                studiedCount++;
+                for (int endNode : graph.get(node)) {
+                    inCount[endNode]--;
+                    // 如果所有先修课程都已经学习
+                    if (inCount[endNode] == 0) {
+                        nextQueue.add(endNode);
+                    }
+                }
+            }
+            bfsQueue = nextQueue;
+        }
+
+        // 检查是否学习所有课程
+        return studiedCount == N ? step : -1;
+
+    }
