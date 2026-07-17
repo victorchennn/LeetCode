d7bf017b23a97d07012ebdf5deb0d51ef698c200
Victor <36978599+victorchennn@users.noreply.github.com>
2026-07-17 14:07:49 -0400

    Update ParallelCourses.java

--- Companies/Bloomberg/ParallelCourses.java
+++ Companies/Bloomberg/ParallelCourses.java
@@ -36,5 +36,35 @@ public int minimumSemesters(int n, int[][] relations) {
 
         // 检查是否学习所有课程
         return studiedCount == N ? step : -1;
+}
 
+// Return the minimum number of months needed to complete all the courses.
+    public int minimumTime(int n, int[][] relations, int[] time) {
+        int mx = 0;
+        List<Integer>[] prev = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            prev[i] = new ArrayList<Integer>();
+        }
+        for (int[] relation : relations) {
+            int x = relation[0], y = relation[1];
+            prev[y].add(x);
+        }
+        Map<Integer, Integer> memo = new HashMap<Integer, Integer>();
+        for (int i = 1; i <= n; i++) {
+            mx = Math.max(mx, dp(i, time, prev, memo));
+        }
+        return mx;
+    }
+
+
+    public int dp(int i, int[] time, List<Integer>[] prev, Map<Integer, Integer> memo) {
+        if (!memo.containsKey(i)) {
+            int cur = 0;
+            for (int p : prev[i]) {
+                cur = Math.max(cur, dp(p, time, prev, memo));
+            }
+            cur += time[i - 1];
+            memo.put(i, cur);
+        }
+        return memo.get(i);
     }
