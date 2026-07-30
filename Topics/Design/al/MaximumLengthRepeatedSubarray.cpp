3c3e0a3b19426c1a506583ec54fdc0da580a28ef
Victor <36978599+victorchennn@users.noreply.github.com>
2026-07-29 21:39:46 -0400

    Update MaximumLengthRepeatedSubarray.cpp

--- Topics/Design/al/MaximumLengthRepeatedSubarray.cpp
+++ Topics/Design/al/MaximumLengthRepeatedSubarray.cpp
@@ -1,3 +1,8 @@
+// Example 1:
+
+// Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+// Output: 3
+// Explanation: The repeated subarray with maximum length is [3,2,1].
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
@@ -42,5 +47,30 @@ public:
             }
             maxLength = max(maxLength, dp[j]);
         }
+    }
+};
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int m = text1.size();
+        int n = text2.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    );
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
