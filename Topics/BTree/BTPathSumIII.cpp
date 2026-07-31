806cd7796e2cf722887899978973aa060caa3c39
Victor <36978599+victorchennn@users.noreply.github.com>
2026-07-31 19:58:01 -0400

    Update BTPathSumIII.cpp

--- Topics/BTree/BTPathSumIII.cpp
+++ Topics/BTree/BTPathSumIII.cpp
@@ -1,3 +1,6 @@
+// Given the root of a binary tree and an integer targetSum, 
+// return the number of paths where the sum of the values along the path equals targetSum.
+
 class Solution {
 public:
     int pathSum(TreeNode* root, int targetSum) {
