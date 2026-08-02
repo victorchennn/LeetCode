9182839429c32eeca864cd22f8b9040b14900b3b
Victor <36978599+victorchennn@users.noreply.github.com>
2026-08-02 16:33:14 -0400

    Create Maximum_Product_Three_Numbers.cpp

---
+++ Topics/Design/al/Maximum_Product_Three_Numbers.cpp
@@ -0,0 +1,28 @@
+// 找出一个数组中乘积最大的三个数
+int maximumProduct(vector<int>& nums) {
+    int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+    int min1 = INT_MAX, min2 = INT_MAX;
+
+    for (int n : nums) {
+        if (n > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = n;
+        } else if (n > max2) {
+            max3 = max2;
+            max2 = n;
+        } else if (n > max3) {
+            max3 = n;
+        }
+
+        if (n < min1) {
+            min2 = min1;
+            min1 = n;
+        } else if (n < min2) {
+            min2 = n;
+        }
+    }
+
+    return max(max1 * max2 * max3,
+               max1 * min1 * min2);
+}
