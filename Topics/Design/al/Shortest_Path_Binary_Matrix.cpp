e89f40ee50b723c3f57107896fbed09bbd43ab85
Victor <36978599+victorchennn@users.noreply.github.com>
2026-08-02 23:44:44 -0400

    Update and rename ShortestPathInBinaryMatrix.cpp to Shortest_Path_Binary_Matrix.cpp

--- Topics/Design/al/Shortest_Path_Binary_Matrix.cpp
+++ Topics/Design/al/Shortest_Path_Binary_Matrix.cpp
@@ -1,7 +1,12 @@
-#include <array>
-#include <queue>
-#include <utility>
-#include <vector>
+// Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+// A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+
+// All the visited cells of the path are 0.
+// All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
+
+// The length of a clear path is the number of visited cells of this path.
+// Input: grid = [[0,0,0],[1,1,0],[1,1,0]]
+// Output: 4
 
 class Solution {
 public:
