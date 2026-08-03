eac99692c0eaa1304c4cecc2fd843d2d5953ebb9
Victor <36978599+victorchennn@users.noreply.github.com>
2026-08-02 23:22:20 -0400

    Update Intersection_Multiple_Arrays.cpp

--- Topics/Design/al/Intersection_Multiple_Arrays.cpp
+++ Topics/Design/al/Intersection_Multiple_Arrays.cpp
@@ -1,3 +1,8 @@
+// Given a 2D integer array nums where nums[i] is a non-empty array of distinct positive integers,
+// return the list of integers that are present in each array of nums sorted in ascending order.
+// Input: nums = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
+// Output: [3,4]
+
 class Solution {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
