d34b0d73d3811fc323e2a8345ce9898b49079e49
Victor <36978599+victorchennn@users.noreply.github.com>
2026-08-03 21:08:14 -0400

    Update CSV_Transpose.cpp

--- Topics/Design/al/CSV_Transpose.cpp
+++ Topics/Design/al/CSV_Transpose.cpp
@@ -38,7 +38,62 @@ void transposeCSV(istream& in, ostream& out) {
     }
 }
 
-int main() {
-    transposeCSV(cin, cout);
-    return 0;
-}
+// return这个column里面所有value
+// 给一个 CSV 文件，例如：
+// Name,Age,City
+// Alice,20,NY
+// Bob,30,SF
+// Charlie,25,LA
+class CSVReader {
+public:
+    explicit CSVReader(istream& in) {
+        string line;
+
+        // header
+        getline(in, line);
+        data_.push_back(split(line));
+
+        for (int i = 0; i < data_[0].size(); ++i) {
+            index_[data_[0][i]] = i;
+        }
+
+        while (getline(in, line)) {
+            data_.push_back(split(line));
+        }
+    }
+
+    vector<string> getColumn(const string& name) {
+        vector<string> result;
+
+        auto it = index_.find(name);
+        if (it == index_.end()) {
+            return result;
+        }
+
+        int col = it->second;
+
+        for (int i = 1; i < data_.size(); ++i) {
+            if (col < data_[i].size()) {
+                result.push_back(data_[i][col]);
+            }
+        }
+
+        return result;
+    }
+
+private:
+    vector<vector<string>> data_;
+    unordered_map<string, int> index_;
+
+    vector<string> split(const string& line) {
+        vector<string> fields;
+        string field;
+        stringstream ss(line);
+
+        while (getline(ss, field, ',')) {
+            fields.push_back(field);
+        }
+
+        return fields;
+    }
+};
