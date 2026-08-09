18d95c267c198280474c93d186dbebbf1d0c9869
Victor <36978599+victorchennn@users.noreply.github.com>
2026-08-09 15:13:49 -0400

    Update MyHashMap.cpp

--- Topics/Design/op/MyHashMap.cpp
+++ Topics/Design/op/MyHashMap.cpp
@@ -18,9 +18,9 @@ private:
 
     // struct Bucket {
     //     ListNode* head = nullptr;
-    //     mutable std::mutex mutex;
+    //     mutable std::mutex mutex; // 即使当前对象是 const，这个成员也允许被修改
     // };
-    // std::vector<std::unique_ptr<Bucket>> buckets_;
+    // std::vector<std::unique_ptr<Bucket>> buckets_; // std::mutex 是 non-copyable / non-movable, 所以用pointer or even std::array<Bucket, SIZE> buckets_;
     ...
     // Bucket& bucket = *buckets_[hash(key)];
     // std::lock_guard<std::mutex> lock(bucket.mutex);
@@ -38,6 +38,7 @@ public:
     MyHashMap() : buckets(SIZE, nullptr) {}
 
     void put(int key, int value) {
+        // std::unique_lock lock(bucket.mutex);
         int idx = hash(key);
 
         if (!buckets[idx]) {
@@ -54,7 +55,8 @@ public:
     }
 
     int get(int key) {
-        int idx = hash(key);
+        // std::shared_lock lock(bucket.mutex);
+        int idx = hash(key); 
 
         if (!buckets[idx]) {
             return -1;
@@ -70,6 +72,7 @@ public:
     }
 
     void remove(int key) {
+        // std::unique_lock lock(bucket.mutex);
         int idx = hash(key);
 
         if (!buckets[idx]) {
@@ -97,3 +100,28 @@ public:
         }
     }
 };
+
+
+// concurrent hashmap
+static constexpr size_t N = 16;
+struct Shard {
+    std::mutex mutex;
+    std::unordered_map<int, int> map;
+};
+std::array<Shard, N> shards;
+
+
+size_t i = std::hash<int>{}(key) % N;
+Shard& shard = shards[i];
+
+std::lock_guard<std::mutex> lock(shard.mutex);
+
+// if read heavy?
+struct Shard {
+    mutable std::shared_mutex mutex;
+    std::unordered_map<int, int> map;
+};
+std::shared_lock lock(shard.mutex); // read
+std::unique_lock lock(shard.mutex); // write
+
+
